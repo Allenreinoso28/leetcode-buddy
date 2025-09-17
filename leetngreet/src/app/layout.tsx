5112b55b0b743f8358ad93954d44abee42be14d4
Allenreinoso28 <157051548+Allenreinoso28@users.noreply.github.com>
2025-09-17 09:25:50 -0400

    auth0 wrapper made

--- leetngreet/src/app/layout.tsx
+++ leetngreet/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Auth0ProviderWrapper from "./providers/auth0-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Auth0ProviderWrapper>
+            {children}
+          </Auth0ProviderWrapper>
+        </body>
+      </html>
   );
 }
