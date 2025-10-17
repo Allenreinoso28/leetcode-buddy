f72190970dc3d24508c8639870b960b8375cedfd
Allenreinoso28 <157051548+Allenreinoso28@users.noreply.github.com>
2025-10-17 18:32:02 -0400

    Add rating and solution fields to post compose form
    Introduced new fields for post rating and solution in the post compose form, updated form validation schema, and improved code language selection UI. Added new UI components for command palette, dialog, and popover using Radix UI and cmdk libraries. Updated dependencies to include @radix-ui/react-popover and cmdk.

--- leetngreet/src/components/post-compose-form.tsx
+++ leetngreet/src/components/post-compose-form.tsx
@@ -30,6 +30,10 @@ import {
   InputGroupText,
   InputGroupTextarea,
 } from "@/components/ui/input-group"
+
+import { Rating, RatingButton } from "./ui/shadcn-io/rating"
+import { useState } from "react"
+
 import {
   Select,
   SelectContent,
@@ -85,6 +89,14 @@ const formSchema = z.object({
         .string()
         .min(20, "Description must be at least 20 characters.")
         .max(1000, "Description must be at most 100 characters."),
+    post_rating: z
+        .int()
+        .min(0)
+        .max(5),
+    post_solution: z
+        .string()
+        .min(20, "Description must be at least 20 characters.")
+        .max(1000, "Description must be at most 100 characters."),
 })
 
 export function PostComposeForm() {
@@ -96,6 +108,8 @@ export function PostComposeForm() {
     post_problem_difficulty: "",
     post_solution_language: "",
     post_caption: "",
+    post_rating: 1,
+    post_solution: "",
   },
 })
 
@@ -127,6 +141,14 @@ export function PostComposeForm() {
       <CardContent>
         <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
           <FieldGroup>
+
+
+
+
+
+            
+
+
             <Controller
               name="post_title"
               control={form.control}
@@ -168,6 +190,13 @@ export function PostComposeForm() {
                 </Field>
               )}
             />
+
+
+
+
+
+
+
             <Controller
               name="post_caption"
               control={form.control}
@@ -201,6 +230,14 @@ export function PostComposeForm() {
                 </Field>
               )}
             />
+
+
+
+
+
+
+
+
             <Controller
                 name="post_solution_language"
                 control={form.control}
@@ -225,20 +262,81 @@ export function PostComposeForm() {
                         <SelectValue placeholder="Select" />
                         </SelectTrigger>
 
-                        <SelectContent
-                        position="item-aligned"
-                        className="grid grid-cols-3 gap-1 p-2"
-                        >
-                        {CodeLanguages.map((lang) => (
-                            <SelectItem key={lang} value={lang}>
-                            {lang}
-                            </SelectItem>
-                        ))}
+                        <SelectContent className="w-lg p-3">
+                            <div className="grid grid-cols-3 gap-2">
+                                {CodeLanguages.map((lang) => (
+                                <SelectItem key={lang} value={lang}>
+                                    {lang}
+                                </SelectItem>
+                                ))}
+                            </div>
                         </SelectContent>
                     </Select>
                     </Field>
                 )}
                 />
+
+
+
+                <Controller
+                name="post_rating"
+                control={form.control}
+                defaultValue={1} // start with 1 stars
+                render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>Rating</FieldLabel>
+                    <FieldDescription>Rate the difficulty of this problem (1-5).</FieldDescription>
+
+                    <Rating
+                        value={field.value}          // bind value from RHF
+                        onValueChange={field.onChange} // update RHF on change
+                    >
+                        {[1, 2, 3, 4, 5].map((num) => (
+                        <RatingButton key={num} aria-label={`${num} star`} />
+                        ))}
+                    </Rating>
+
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                )}
+                />
+
+                <Controller
+              name="post_caption"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-description">
+                    Description
+                  </FieldLabel>
+                  <InputGroup>
+                    <InputGroupTextarea
+                      {...field}
+                      id="form-rhf-demo-description"
+                      placeholder="I'm having an issue with the login button on mobile."
+                      rows={6}
+                      className="min-h-24 resize-none"
+                      aria-invalid={fieldState.invalid}
+                    />
+                    <InputGroupAddon align="block-end">
+                      <InputGroupText className="tabular-nums">
+                        {field.value.length}/1000 characters
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  <FieldDescription>
+                    Include steps to reproduce, expected behavior, and what
+                    actually happened.
+                  </FieldDescription>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+
+
           </FieldGroup>
         </form>
       </CardContent>
